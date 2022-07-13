4a5020f7bbbec56ee550cacdf555720bf4620b27
Paul Balanche <pb@buzzbrothers.ch>
2022-07-13 10:23:49 +0200

    Wip

---
+++ wpe.rb
@@ -0,0 +1,17 @@
+class WpLocalInstance < Formula
+    desc ""
+    homepage ""
+    url "https://github.com/PaulBalanche/wp-local-instance/archive/refs/heads/master.zip"
+    version "0.0.8"
+    sha256 ""
+    license ""
+  
+    def install
+      system "./configure", *std_configure_args, "--disable-silent-rules"
+    end
+  
+    test do
+      system "false"
+    end
+  end
+  
