6f024881e1430d000cb9a9cadbe425cd8a728f31
Paul Balanche <pb@buzzbrothers.ch>
2022-07-13 10:28:58 +0200

    Up

--- wpe.rb
+++ wpe.rb
@@ -1,9 +1,8 @@
 class WpLocalInstance < Formula
     desc ""
     homepage ""
-    url "https://github.com/PaulBalanche/wp-local-instance/archive/refs/heads/master.zip"
-    version "0.0.9"
-    sha256 ""
+    url "https://github.com/PaulBalanche/wp-local-instance/archive/refs/tags/0.0.9.tar.gz"
+    sha256 "77477bfc56f6486cb08f51ac0edf3a9cae7b779ea04d18e1ce070aab811e483b"
     license ""
   
     def install
